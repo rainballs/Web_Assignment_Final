813a77fc3588f44445cb52234ccafb495f210903
Nikolai Atsev <rainballs.niki@gmail.com>
2024-11-12 23:34:27 +0200

    Added configuration plus views models and forms

--- Web_FInal_Assignment_project/urls.py
+++ Web_FInal_Assignment_project/urls.py
@@ -1,22 +1,21 @@
-"""
-URL configuration for Web_FInal_Assignment_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('register', views.register, name='register'),
+    path('profile/weight', views.weight_log_view, name='weight_log'),
+    path('profile/weight/delete/<int:weight_id>', views.weight_log_delete, name='weight_log_delete'),
+
+    path('food/list', views.food_list_view, name='food_list'),
+    path('food/add', views.food_add_view, name='food_add'),
+    path('food/foodlog', views.food_log_view, name='food_log'),
+    path('food/foodlog/delete/<int:food_id>', views.food_log_delete, name='food_log_delete'),
+    path('food/<str:food_id>', views.food_details_view, name='food_details'),
+
+    path('categories', views.categories_view, name='categories_view'),
+    path('categories/<str:category_name>', views.category_details_view, name='category_details_view'),
 ]
